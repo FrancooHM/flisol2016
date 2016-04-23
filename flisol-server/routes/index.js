0a543b3456fd92f4342461f0946f42b411db6479
Franco Agustín Rabaglia <francoo.hd@gmail.com>
2016-04-23 16:21:01 -0300

    cleaning routes step1.

--- flisol-server/routes/index.js
+++ flisol-server/routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Flisol 2016' });
 });
 
-router.get('/hello', function(req, res, next) {
-  console.log('Hola flisol!');
-  console.log('HEADERS: ');
-  console.log(req.headers);
-  console.log('BODY: ');
-  console.log(req.body);
-  res.end();
-});
 module.exports = router;
